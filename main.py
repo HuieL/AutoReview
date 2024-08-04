83778df82fb81f482059a28c9abe6e10185b7c18
Andrew Choi <choiandrew585@gmail.com>
2024-08-04 16:25:51 -0400

    Change main.py to reflect correct input/output folders

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import os
 path = os.getcwd()
 sys.path.append(os.path.abspath(path + "/src/utils"))
 from find_paper import *
-from llm_model import *
 
 def main():
-    #base_folder = path + "/dataset/ICLR_2017"
-    #output_folder = path + "/dataset/processed"
-    #graphs = process_conference(base_folder)
-    #save_graph(graphs, output_folder, "ICLR_2017")
-    print(torch.load("dataset/processed/ICLR_2017.pt"))
+    base_folder = path + "/dataset"
+    output_folder = path + "/processed"
+    process_conferences(base_folder, output_folder)
+    print(torch.load(path + "/processed/ICLR_2017.pt"))
     return 0
 
 
